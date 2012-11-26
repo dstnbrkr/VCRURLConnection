5e164fcc6e8bc61c7be549f6a34f890bdb507153
Dustin Barker <dustin.barker@gmail.com>
2012-11-25 16:10:32 -0800

    Test that recorded data matches received data

---
+++ Tests/Tests/VCRURLConnectionTests.h
@@ -0,0 +1,13 @@
+//
+//  VCRURLConnectionTests.h
+//  Tests
+//
+//  Created by Dustin Barker on 11/25/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface VCRURLConnectionTests : SenTestCase
+
+@end
---
+++ Tests/Tests/VCRURLConnectionTests.m
@@ -0,0 +1,83 @@
+//
+//  VCRURLConnectionTests.m
+//  Tests
+//
+//  Created by Dustin Barker on 11/25/12.
+//
+//
+
+#import "VCRURLConnectionTests.h"
+#import "VCRCassetteManager.h"
+#import "VCRCassette.h"
+
+@interface VCRURLConnectionTestDelegate : NSObject<NSURLConnectionDelegate>
+@property (nonatomic, retain) NSData *data;
+@property (assign, getter = isDone) BOOL done;
+@end
+
+
+@interface VCRURLConnectionTests ()
+@property (nonatomic, retain) VCRCassette *cassette;
+@property (nonatomic, retain) VCRURLConnectionTestDelegate *testDelegate;
+
+@end
+
+
+@implementation VCRURLConnectionTests
+
+- (void)setUp {
+    [super setUp];
+    self.cassette = [[VCRCassetteManager defaultManager] cassette];
+    self.testDelegate = [[[VCRURLConnectionTestDelegate alloc] init] autorelease];
+}
+
+- (void)tearDown {
+    self.cassette = nil;
+    self.testDelegate = nil;
+    [super tearDown];
+}
+
+- (void)testAsyncConnectionRecordedAndReplayed {
+    NSURL *url = [NSURL URLWithString:@"http://www.iana.org/domains/example/"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    // request has not been recorded yet
+    STAssertNil([self.cassette responseForRequest:request], @"Should not have recording for request yet");
+    
+    // make and record request
+    [NSURLConnection connectionWithRequest:request delegate:self.testDelegate];
+    
+    // wait for request to finish
+    while (![self.testDelegate isDone]) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+        usleep(10000);
+    }
+    
+    // the response was recorded
+    VCRResponse *recordedResponse = [self.cassette responseForRequest:request];
+    NSData *receivedData = self.testDelegate.data;
+    NSData *recordedData = recordedResponse.responseData;
+    STAssertEqualObjects(recordedData, receivedData, @"Recorded data should equal received data");
+}
+
+// FIXME: test with enough data to fire connection:didReceiveData: times (test data appending)
+
+@end
+
+
+@implementation VCRURLConnectionTestDelegate
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    self.data = data;
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    _done = YES;
+}
+
+- (void)dealloc {
+    self.data = nil;
+    [super dealloc];
+}
+
+@end
