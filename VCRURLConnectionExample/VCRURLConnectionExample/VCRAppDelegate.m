6dd26ea0e03f902fea3e3493979bb835dd343a70
Dustin Barker <dustin.barker@gmail.com>
2012-12-09 15:28:28 -0800

    Whitespace

--- VCRURLConnectionExample/VCRURLConnectionExample/VCRAppDelegate.m
+++ VCRURLConnectionExample/VCRURLConnectionExample/VCRAppDelegate.m
@@ -21,13 +21,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-    // Override point for customization after application launch.
-    self.viewController = [[[VCRViewController alloc] initWithNibName:@"VCRViewController" bundle:nil] autorelease];
-    self.window.rootViewController = self.viewController;
-    [self.window makeKeyAndVisible];
+    
     [VCR start];
     NSString *cassettePath = [[NSBundle mainBundle] pathForResource:@"cassette" ofType:@"json"];
     [VCR setCassetteURL:[NSURL fileURLWithPath:cassettePath]];
+    
+    self.viewController = [[[VCRViewController alloc] initWithNibName:@"VCRViewController" bundle:nil] autorelease];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+
     return YES;
 }
 
