874cf9e30cf637cd4da0706b410f9277ca2c5570
Dustin Barker <dustin.barker@gmail.com>
2012-12-09 15:47:25 -0800

    Stub out cassette view controller

---
+++ VCRURLConnectionExample/VCRURLConnectionExample/VCRCassetteViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCRCassetteViewController.h
+//  VCRURLConnectionExample
+//
+//  Created by Dustin Barker on 12/9/12.
+//  Copyright (c) 2012 Dustin Barker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCRCassetteViewController : UITableViewController
+
+@end
