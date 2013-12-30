3b46da98878695f227705f270128a02a0428918e
Dustin Barker <dustin.barker@gmail.com>
2013-12-30 02:01:40 -0500

    Fix import order

--- VCRURLConnection/VCR+NSURLSession.h
+++ VCRURLConnection/VCR+NSURLSession.h
@@ -21,10 +21,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <Availability.h>
-
-
 #import "VCR.h"
+#import <Availability.h>
 
 @interface VCR (NSURLSession)
 
