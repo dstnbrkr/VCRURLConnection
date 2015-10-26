fefcce81c7d79230f11f79a7c565d0130497c8c3
Dustin Barker <dstnbrkr@seed.co>
2015-10-25 19:49:28 -0700

    Build VCRURLConnection as framework

---
+++ VCRURLConnection/VCRURLConnection.h
@@ -0,0 +1,19 @@
+//
+//  VCRURLConnection.h
+//  VCRURLConnection
+//
+//  Created by Dustin Barker on 10/25/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VCRURLConnection.
+FOUNDATION_EXPORT double VCRURLConnectionVersionNumber;
+
+//! Project version string for VCRURLConnection.
+FOUNDATION_EXPORT const unsigned char VCRURLConnectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VCRURLConnection/PublicHeader.h>
+
+
