ec93b10e85d61784439f5bc409f55ebbfb70289b
Matt Ryan <matt@souther.ly>
2016-11-04 14:45:36 +1100

    Adds iOS framework for use with Carthage. Updates project README.

--- VCRURLConnection/VCRURLConnection.h
+++ VCRURLConnection/VCRURLConnection.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char VCRURLConnectionVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <VCRURLConnection/PublicHeader.h>
 
-
+#import "VCR.h"
